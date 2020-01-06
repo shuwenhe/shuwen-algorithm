3ac2a8a2d3a3a8bbd943cd121d7c81ee1f106efb
Chenghui Lei <jeeny520@126.com>
2020-01-06 20:54:11 +0800

    add

--- code/string/main.go
+++ code/string/main.go
@@ -1,3 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
 // 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
 // 示例 1:
 // 输入: "abcabcbb"
@@ -12,18 +19,6 @@
 // 输出: 3
 // 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
 //      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
-
-package main
-
-import (
-	"fmt"
-	"strings"
-)
-
-func main() {
-	i := lengthOfLongestSubstring("abcabcbb")
-	fmt.Println(i)
-}
 func lengthOfLongestSubstring(s string) int {
 	var max, start, end, length = 0, 0, 0, len(s)
 	for start < length && end < length {
@@ -37,4 +32,19 @@ func lengthOfLongestSubstring(s string) int {
 		}
 	}
 	return max
+}
+
+func main1() {
+	i := lengthOfLongestSubstring("abcabcbb")
+	fmt.Println(i)
+}
+
+// 字符串拼接
+func stringAdd()  {
+	str := "Richard"+"&"+"Ritchie"
+	fmt.Println("str = ",str)
+}
+
+func main()  {
+	stringAdd()
 }
