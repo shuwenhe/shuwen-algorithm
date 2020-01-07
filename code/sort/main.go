b3db219967f0b6dc1b6742252a737cfb2a44718b
Chenghui Lei <jeeny520@126.com>
2020-01-07 15:18:03 +0800

    add

--- code/sort/main.go
+++ code/sort/main.go
@@ -2,7 +2,41 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
+
+// quick sort
+func main()  {
+	var z []int
+
+	for i := 0; i < 3; i++{
+		z = append(z, rand.Intn(3))
+	}
+	
+	quickSort(z)	
+}
+
+func quickSort(list []int)  {
+	if len(list) <= 1{
+		return
+	}
+
+	i, j := 0, len(list) - 1
+	index := 1 // 第一次比较索引位置
+	key := list[0] // 第一次比较参考值，选择第一个
+
+	if list[index] > key{
+		list[i], list[j] = list[j], list[i]
+		j--
+	}else{
+		list[i], list[index] = list[index],list[i]
+		i++
+		index++
+	}
+
+	quickSort(list[:i]) // 处理参考值前面值
+	quickSort(list[i+1:])
+}
 
 // selection sort
 func main1() {
@@ -17,16 +51,5 @@ func SelectionSort(s []int) {
 	fmt.Println(l)
 }
 
-// quick sort
-func main()  {
-	var z []int
-	quickSort(z)	
-}
 
-func quickSort(list []int)  {
-	if len(list) <= 1{
-		return
-	}
-	fmt.Println(quickSort)
-}
 
