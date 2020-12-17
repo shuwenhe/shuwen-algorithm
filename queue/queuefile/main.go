0d7121fbea45e517f266da47cc19321ec754679b
ShuwenHe <1201220707@pku.edu.cn>
2020-12-17 17:04:47 +0800

    fix: queue

--- queue/queuefile/main.go
+++ queue/queuefile/main.go
@@ -6,7 +6,7 @@ import (
 
 // MyQueue Interface
 type MyQueue interface {
-	Size() int                // 队列的大小
+	GetSize() int             // 队列的大小
 	Front() interface{}       // 队列的第一个元素
 	End() interface{}         // 队列的最后一个元素
 	IsEmpty() bool            // 判断队列是否位空
@@ -17,13 +17,13 @@ type MyQueue interface {
 
 // Queue Struct
 type Queue struct {
-	dataStore []interface{} // 队列数据的存储
-	theSize   int           // 队列的大小
+	Data []interface{} // 队列数据的存储
+	Size int           // 队列的大小
 }
 
 func main() {
 	fmt.Println("queue")
-	myq := NewQueue()
+	myq := Queue{}
 
 	myq.EnQueue(1)
 	myq.EnQueue(2)
@@ -41,61 +41,53 @@ func main() {
 	fmt.Println("IsEmpty = ", myq.IsEmpty())
 }
 
-// NewQueue New queue
-func NewQueue() *Queue {
-	myQueue := new(Queue)
-	myQueue.dataStore = make([]interface{}, 0)
-	myQueue.theSize = 0
-	return myQueue
-}
-
 // Size 队列的大小
-func (myq *Queue) Size() int {
-	return myq.theSize
+func (myq *Queue) GetSize() int {
+	return myq.Size
 }
 
 // Front 队列的第一个元素
 func (myq *Queue) Front() interface{} {
-	if myq.theSize == 0 {
+	if myq.Size == 0 {
 		return nil
 	}
-	return myq.dataStore[0]
+	return myq.Data[0]
 }
 
 // End 队列的最后一个元素
 func (myq *Queue) End() interface{} {
-	if myq.theSize == 0 {
+	if myq.Size == 0 {
 		return nil
 	}
-	return myq.dataStore[myq.Size()-1]
+	return myq.Data[myq.GetSize()-1]
 }
 
 // IsEmpty 判断队列是否位空
 func (myq *Queue) IsEmpty() bool {
-	return myq.theSize == 0
+	return myq.Size == 0
 }
 
 // EnQueue 入队
 func (myq *Queue) EnQueue(data interface{}) {
-	myq.dataStore = append(myq.dataStore, data)
-	myq.theSize++
+	myq.Data = append(myq.Data, data)
+	myq.Size++
 }
 
 // DeQueue 出队
 func (myq *Queue) DeQueue() interface{} {
-	if myq.theSize == 0 {
+	if myq.Size == 0 {
 		return nil
 	}
-	data := myq.dataStore[0]
-	if myq.Size() > 1 {
-		myq.dataStore = myq.dataStore[1:myq.Size()] // 对切片进行截取
+	data := myq.Data[0]
+	if myq.GetSize() > 1 {
+		myq.Data = myq.Data[1:myq.GetSize()] // 对切片进行截取
 	}
-	myq.theSize--
+	myq.Size--
 	return data
 }
 
 // Clear 清空数据
 func (myq *Queue) Clear() {
-	myq.dataStore = make([]interface{}, 0)
-	myq.theSize = 0
+	myq.Data = make([]interface{}, 0)
+	myq.Size = 0
 }
