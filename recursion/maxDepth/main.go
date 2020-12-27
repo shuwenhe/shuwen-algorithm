bae2cf3138fff7158fa4d34e0244672ec3385735
ShuwenHe <1201220707@pku.edu.cn>
2020-12-27 10:19:46 +0800

    feat: maxDepth

---
+++ recursion/maxDepth/main.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	r7 := &TreeNode{
+		Val:   7,
+		Left:  nil,
+		Right: nil,
+	}
+
+	l15 := &TreeNode{
+		Val:   15,
+		Left:  nil,
+		Right: nil,
+	}
+
+	l9 := &TreeNode{
+		Val:   9,
+		Left:  nil,
+		Right: nil,
+	}
+
+	r20 := &TreeNode{
+		Val:   9,
+		Left:  l15,
+		Right: r7,
+	}
+
+	root := &TreeNode{
+		Val:   3,
+		Left:  l9,
+		Right: r20,
+	}
+
+	fmt.Println(maxDepth(root))
+}
