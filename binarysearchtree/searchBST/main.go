971aa5ed59c457dbf6cebd5187da047fe7c82056
ShuwenHe <1201220707@pku.edu.cn>
2020-12-27 14:23:26 +0800

    feat: intersect

---
+++ binarysearchtree/searchBST/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	return nil
+}
+
+func main() {
+	fmt.Println("searchBST")
+}
