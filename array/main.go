e0216c2549210d720450ef4675e263b11e05e0f0
ShuwenHe <1201220707@pku.edu.cn>
2020-12-30 23:54:18 +0800

    feat: smallerNumbersThanCurrent

--- array/main.go
+++ array/main.go
@@ -1,40 +1,44 @@
+// 给你一个整数数组 nums 。
+// 如果一组数字 (i,j) 满足 nums[i] == nums[j] 且 i < j ，就可以认为这是一组 好数对 。
+// 返回好数对的数目。
+// 示例 1：
+// 输入：nums = [1,2,3,1,1,3]
+// 输出：4
+// 解释：有 4 组好数对，分别是 (0,3), (0,4), (3,4), (2,5) ，下标从 0 开始
+// 示例 2：
+// 输入：nums = [1,1,1,1]
+// 输出：6
+// 解释：数组中的每组数字都是好数对
+// 示例 3：
+// 输入：nums = [1,2,3]
+// 输出：0
+// 提示：
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+
 package main
 
-import (
-	"algorithm/array/arraylist"
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	var list arraylist.List = arraylist.NewArrayList()
-	list.Append("a")
-	list.Append("b")
-	list.Append("c")
-	list.Append("d")
-	list.Append("e")
-	for it := list.Iterator(); it.HasNext(); {
-		item, _ := it.Next("123456")
-		if item == "d" {
-			it.Remove()
+func numIdenticalPairs(nums []int) int {
+	var index1, index2 int
+	var r int
+	for index1 < len(nums) && index2 < len(nums) {
+		if index1 < index2 {
+			if nums[index1] == nums[index2] {
+				r++ // 双指针遍历数组，符合条件结果+1
+			}
 		}
-		fmt.Println(item)
+		index2++                 // 每次循环将2指针+1
+		if index2 == len(nums) { // 判断2指针边界，超出边界
+			index1++            //  将1指针进位
+			index2 = index1 + 1 // 重置2指针为1指针的+1
+		}
 	}
-	fmt.Println(list)
+	return r
 }
 
-func main2() {
-	// list := NewArrayList()
-	var list arraylist.List = arraylist.NewArrayList() // 定义接口对象，赋值的对象必须实现接口的所有方法
-	list.Append(1)
-	list.Append(2)
-	list.Append(3)
-	list.Append("a")
-	list.Append("b")
-	list.Append("c")
-	for i := 0; i < 3; i++ {
-		list.Insert(2, "val7")
-		fmt.Println("list-insert = ", list)
-	}
-	list.Delete(2) // CRUD
-	fmt.Println("list-delete = ", list)
+func main() {
+	nums := []int{1, 2, 3, 1, 1, 3}
+	fmt.Println(numIdenticalPairs(nums))
 }
---
+++ sort/quicksort/smallerNumbersThanCurrent/main.go
@@ -0,0 +1,57 @@
+// 给你一个数组 nums，对于其中每个元素 nums[i]，请你统计数组中比它小的所有数字的数目。
+// 换而言之，对于每个 nums[i] 你必须计算出有效的 j 的数量，其中 j 满足 j != i 且 nums[j] < nums[i] 。
+// 以数组形式返回答案。
+// 示例 1：
+// 输入：nums = [8,1,2,2,3]
+// 输出：[4,0,1,1,3]
+// 解释：
+// 对于 nums[0]=8 存在四个比它小的数字：（1，2，2 和 3）。
+// 对于 nums[1]=1 不存在比它小的数字。
+// 对于 nums[2]=2 存在一个比它小的数字：（1）。
+// 对于 nums[3]=2 存在一个比它小的数字：（1）。
+// 对于 nums[4]=3 存在三个比它小的数字：（1，2 和 2）。
+// 示例 2：
+// 输入：nums = [6,5,4,8]
+// 输出：[2,1,0,3]
+// 示例 3：
+// 输入：nums = [7,7,7,7]
+// 输出：[0,0,0,0]
+// 提示：
+// 2 <= nums.length <= 500
+// 0 <= nums[i] <= 100
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type pair struct {
+	v, pos int
+}
+
+func smallerNumbersThanCurrent(nums []int) []int {
+	n := len(nums)
+	data := make([]pair, n)
+	for i, v := range nums {
+		data[i] = pair{v, i}
+	}
+	sort.Slice(data, func(i, j int) bool {
+		return data[i].v < data[j].v
+	})
+	ans := make([]int, n)
+	prev := -1
+	for i, d := range data {
+		if prev == -1 || d.v != data[i-1].v {
+			prev = i
+		}
+		ans[d.pos] = prev
+	}
+	return ans
+}
+
+func main() {
+	nums := []int{8, 1, 2, 2, 3}
+	fmt.Println(smallerNumbersThanCurrent(nums))
+}
